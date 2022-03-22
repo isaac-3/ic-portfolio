989c5b8b86515d62f115d8c4632e5c107b4ee233
isaac-3 <isaacgc0596@gmail.com>
2022-03-22 01:02:54 -0500

    create about section

--- components/about.tsx
+++ components/about.tsx
@@ -1,11 +1,37 @@
 import React from "react";
+import Image from "next/image";
 
-type Props = {};
-
-const About = (props: Props) => {
+const About = () => {
   return (
     <div className="about-container" id="about">
-      About
+      <div className="about-me-name">
+        <Image
+          alt="profile-pic"
+          src="/croppro.jpg"
+          className="about-profile"
+          width={120}
+          height={150}
+        />
+        <h1>hi my name is isaac chavez and i am a . . .</h1>
+        <span
+          className="txt-type"
+          data-wait="3000"
+          data-words='["Software Developer", "Lifelong Learner", "Problem Solver"]'
+        ></span>
+      </div>
+      <div className="about-seperater"></div>
+      <div className="about-me-read">
+        <h3>
+          Since I began my journey as a software engineer, I have been
+          captivated by everything about it. Working with and learning new
+          technologies Intrigues me and gives me new ideas and perspectives to
+          various solutions. As a developer, you learn that there is more to
+          programming than just coding, it's an opportunity to help and improve
+          the world around us. Knowing that software development is a
+          never-ending road of learning, I am excited to see what it may and
+          will evolve to.
+        </h3>
+      </div>
     </div>
   );
 };
