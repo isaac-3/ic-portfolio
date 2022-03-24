b033cfd99008478fe25d03ea0c6ebac26121b046
isaac-3 <isaacgc0596@gmail.com>
2022-03-24 14:44:27 -0500

    add skills

--- components/skills.tsx
+++ components/skills.tsx
@@ -1,9 +1,37 @@
 import React from "react";
 
-type Props = {};
-
-const Skills = (props: Props) => {
-  return <div>Skills</div>;
+const Skills = () => {
+  return (
+    <div className="skills-container" id="skills">
+      <div className="skills-header">
+        <h1>TECHNOLOGIES</h1>
+      </div>
+      <div className="skill-box">
+        <i className="devicon-javascript-plain colored" data-speed="-5"></i>
+        <i className="devicon-typescript-plain colored"></i>
+        <i
+          className="devicon-react-original-wordmark colored"
+          data-speed="5"
+        ></i>
+        <i className="devicon-nextjs-original-wordmark colored"></i>
+        <i className="devicon-nodejs-plain-wordmark colored"></i>
+        <i className="devicon-html5-plain-wordmark colored" data-speed="2"></i>
+        <i className="devicon-css3-plain-wordmark colored" data-speed="6"></i>
+        <i className="devicon-sass-original colored"></i>
+        <i className="devicon-ruby-plain-wordmark colored" data-speed="8"></i>
+        <i className="devicon-rails-plain-wordmark colored" data-speed="-2"></i>
+        <i className="devicon-mongodb-plain-wordmark colored"></i>
+        <i className="devicon-postgresql-plain-wordmark colored"></i>
+        <i className="devicon-redux-original colored" data-speed="-7"></i>
+        <i className="devicon-express-original-wordmark colored"></i>
+        <i
+          className="devicon-github-original-wordmark colored"
+          data-speed="-9"
+        ></i>
+        <i className="devicon-python-plain-wordmark colored" data-speed="6"></i>
+      </div>
+    </div>
+  );
 };
 
 export default Skills;
