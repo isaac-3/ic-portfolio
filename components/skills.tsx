9343441d606125828ad5d8a8decd3f372ec2747e
isaac-3 <isaacgc0596@gmail.com>
2022-03-24 19:04:44 -0500

    add stlyling sheets in header

--- components/skills.tsx
+++ components/skills.tsx
@@ -7,28 +7,23 @@ const Skills = () => {
         <h1>TECHNOLOGIES</h1>
       </div>
       <div className="skill-box">
-        <i className="devicon-javascript-plain colored" data-speed="-5"></i>
+        <i className="devicon-javascript-plain colored"></i>
         <i className="devicon-typescript-plain colored"></i>
-        <i
-          className="devicon-react-original-wordmark colored"
-          data-speed="5"
-        ></i>
+        <i className="devicon-react-original-wordmark colored"></i>
         <i className="devicon-nextjs-original-wordmark colored"></i>
         <i className="devicon-nodejs-plain-wordmark colored"></i>
-        <i className="devicon-html5-plain-wordmark colored" data-speed="2"></i>
-        <i className="devicon-css3-plain-wordmark colored" data-speed="6"></i>
+        <i className="devicon-html5-plain-wordmark colored"></i>
+        {/* <i className="devicon-css3-plain-wordmark colored" ></i> */}
+        <i className="devicon-graphql-plain-wordmark colored"></i>
         <i className="devicon-sass-original colored"></i>
-        <i className="devicon-ruby-plain-wordmark colored" data-speed="8"></i>
-        <i className="devicon-rails-plain-wordmark colored" data-speed="-2"></i>
+        <i className="devicon-ruby-plain-wordmark colored"></i>
+        <i className="devicon-rails-plain-wordmark colored"></i>
         <i className="devicon-mongodb-plain-wordmark colored"></i>
         <i className="devicon-postgresql-plain-wordmark colored"></i>
-        <i className="devicon-redux-original colored" data-speed="-7"></i>
+        <i className="devicon-redux-original colored"></i>
         <i className="devicon-express-original-wordmark colored"></i>
-        <i
-          className="devicon-github-original-wordmark colored"
-          data-speed="-9"
-        ></i>
-        <i className="devicon-python-plain-wordmark colored" data-speed="6"></i>
+        <i className="devicon-github-original-wordmark colored"></i>
+        <i className="devicon-python-plain-wordmark colored"></i>
       </div>
     </div>
   );
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,28 @@
-import type { AppProps } from 'next/app'
-import "../styles/style.scss"
+/* eslint-disable @next/next/no-page-custom-font */
+import type { AppProps } from "next/app";
+import Head from "next/head";
+import "../styles/style.scss";
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Head>
+        <link
+          rel="stylesheet"
+          href="https://cdn.jsdelivr.net/gh/konpa/devicon@master/devicon.min.css"
+        />
+        <link
+          rel="stylesheet"
+          href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css"
+        />
+        <link
+          rel="stylesheet"
+          href="https://fonts.googleapis.com/css2?family=Advent+Pro:wght@300&family=Poppins:wght@200&display=swap"
+        />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  );
 }
 
-export default MyApp
+export default MyApp;
