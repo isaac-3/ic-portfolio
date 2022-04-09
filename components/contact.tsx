388709123fa3631f6600d657b48eee53f593420f
isaac-3 <isaacgc0596@gmail.com>
2022-04-09 01:55:31 -0500

    clean up contact form

--- components/contact.tsx
+++ components/contact.tsx
@@ -45,42 +45,39 @@ const Contact = () => {
         </div>
       </div>
       <div className="contact-form">
-        <div className="form-title">
-          <h1>Get In Touch</h1>
-        </div>
+        <h1>Get In Touch</h1>
         <form name="contact" method="post" data-netlify="true">
           <input type="hidden" name="form-name" value="contact" />
-          <label className="label">Name</label>
+          <label className="contact-input-label">Name</label>
           <input
             required
-            className="input"
+            className="contact-input"
             type="text"
             placeholder="First Last"
             name="name"
           />
           <br />
           <br />
-          <label className="label">Email</label>
+          <label className="contact-input-label">Email</label>
           <input
             required
-            className="input"
+            className="contact-input"
             type="email"
             placeholder="ex@example.com"
             name="email"
           />
           <br />
           <br />
-          <label className="label">Message</label>
+          <label className="contact-input-label">Message</label>
           <textarea
             required
-            className="input"
-            id="message-box"
+            className="message-box"
             placeholder="Enter Message"
             name="message"
           />
           <br />
           <br />
-          <button className="red" type="submit">
+          <button className="contact-form-submit" type="submit">
             Send
           </button>
         </form>
