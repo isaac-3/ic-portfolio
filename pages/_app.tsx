d86d619a537d6e220b6a08dd989ad28dd70368ae
isaac-3 <isaacgc0596@gmail.com>
2022-12-10 14:21:31 -0600

    test3

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,11 +1,12 @@
 /* eslint-disable @next/next/no-page-custom-font */
 import type { AppProps } from "next/app";
 import Head from "next/head";
+import { ErrorBoundary } from "../components/errorBounty";
 import "../styles/style.scss";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <>
+    <ErrorBoundary>
       <Head>
         <title>Isaac Chavez</title>
         <link rel="icon" href="iclogo.svg" />
@@ -56,7 +57,7 @@ function MyApp({ Component, pageProps }: AppProps) {
         /> */}
       </Head>
       <Component {...pageProps} />
-    </>
+    </ErrorBoundary>
   );
 }
 
